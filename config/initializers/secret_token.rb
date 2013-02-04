4ab97867f64e97f62fab2a77b5621f251c1aeb13
brownmagik352 <asuman@dhcp-128-36-57-26.wireless.yale.edu>
2013-02-03 22:29:23 -0500

    initial commit and first view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventbook::Application.config.secret_token = 'f3a0541cc9380271d6ec639bf93dabb98bff87a628f4b40f27d13d7efabfc8c34e69d5091cac9016ced4bd14b423f42562eb10e628462dc0848fac14f3fc54ca'
