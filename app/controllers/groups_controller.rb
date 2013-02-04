8be9500992d8ae9b86440ace736f0d350f23b446
brownmagik352 <asuman@dhcp-128-36-57-26.wireless.yale.edu>
2013-02-03 23:58:43 -0500

    made group model

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,23 @@
 class GroupsController < ApplicationController
-	
+
 	def index
+		@groups = Group.all
 	end
+
+	def new
+		@group = Group.new
+	end
+
+	def create
+		@group = Group.new(params[:group])
+		@group.save
+		redirect_to groups_path
+	end
+
+	def show
+		@group = Group.find(params[:id])
+	end
+
+
 
 end
